ef4db74788096900dce27cde5d9fc1cd1408992e
Touched <jtouched@gmail.com>
2015-06-20 18:26:00 +0200

    Renamed symbols

--- src/healthbar.c
+++ src/healthbar.c
@@ -3,19 +3,19 @@
 #include "images/indicators.h"
 
 //resource gfx_healthbar = {0x083EF524, 0x80, 0x1234};
-resource gfx_healthbar = {indicatorsTiles, 0x80, 0x1234};
+resource gfx_indicator = {indicatorsTiles, 0x80, 0x1234};
 resource gfx_trigger = {0x083EF524, 0x1C00, 0x2345};
 resource pal_indicator = {indicatorsPal, 0x1234};
 // 083F6CBO - 32x32?
 
-sprite mega_icon = {0, 0x0, 0x000, 0x0};
+sprite mega_indicator = {0, 0x0, 0x000, 0x0};
 //sprite mega_icon = {0x0, 0x4000, 0x000, 0x0};
 sprite mega_trigger = {0, 0x8000, 0x800, 0};
 
 void healthbar_trigger_callback(object *self);
 void healthbar_indicator_callback(object *self);
 
-template template_healthbar = {0x1234, 0x1234, &mega_icon, 0x08231CF0, 0, 0x08231CFC, healthbar_indicator_callback};
+template template_indicator = {0x1234, 0x1234, &mega_indicator, 0x08231CF0, 0, 0x08231CFC, healthbar_indicator_callback};
 template template_trigger = {0x2345, 0xD6FF, &mega_trigger, 0x08231CF0, 0, 0x08231CFC, healthbar_trigger_callback};
 
 /*
@@ -86,8 +86,8 @@ void healthbar_load_graphics(u8 state) {
 	if (state == 2) {
 		gpu_pal_obj_alloc_tag_and_apply(&pal_indicator);
 	
-		gpu_tile_obj_decompress_alloc_tag_and_upload(&gfx_healthbar);
-		template_instanciate_forward_search(&template_healthbar, 90, 25, 1);
+		gpu_tile_obj_decompress_alloc_tag_and_upload(&gfx_indicator);
+		template_instanciate_forward_search(&template_indicator, 90, 25, 1);
 		gpu_tile_obj_decompress_alloc_tag_and_upload(&gfx_trigger);
 		template_instanciate_forward_search(&template_trigger, 130, 90, 1);
 	}
