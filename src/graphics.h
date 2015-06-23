0f1d1cab432f6b40643025a464beda313ede188a
Touched <jtouched@gmail.com>
2015-06-23 18:36:19 +0200

    Graphics header

---
+++ src/graphics.h
@@ -0,0 +1,19 @@
+#ifndef GRAPHICS_H_
+#define GRAPHICS_H_
+
+typedef struct colour {
+	u16 r : 5;
+	u16 g : 5;
+	u16 b : 5;
+} color;
+
+typedef struct palette {
+	color c[16];
+} palette;
+
+palette *palette_bg_unfaded = (palette*) 0x020371F8;
+palette *palette_obj_unfaded = (palette*) 0x020373F8;
+palette *palette_bg_faded = (palette*) 0x020375F8;
+palette *palette_obj_faded = (palette*) 0x020377F8;
+
+#endif
