1c51a3d31d44135e5b6e7e9d53c9e3237e892482
Touched <jtouched@gmail.com>
2015-07-24 20:15:36 +0200

    Removed trainer.h

--- src/images/indicators.c
+++ src/images/indicators.c
@@ -9,7 +9,7 @@
 //	+ 3 tiles lz77 compressed
 //	Total size: 32 + 112 = 144
 //
-//	Time-stamp: 2015-07-24, 20:10:56
+//	Time-stamp: 2015-07-24, 20:14:53
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.12
 //	( http://www.coranac.com/projects/#grit )
 //
